f5013c8afee1db8d2824312f94b154f88911bc0a
Kyle Cutler <kyle1320@gmail.com>
2019-07-17 18:46:28 -0400

    Remove per-attribute build steps

--- src/common/build/BuildSystem.ts
+++ src/common/build/BuildSystem.ts
@@ -1,5 +1,4 @@
 import LObject from "../data/LObject";
-import Attribute from "../data/Attribute";
 import Project from "../data/Project";
 
 type BuildStep<T> = (arg: T) => any;
@@ -7,9 +6,7 @@ type BuildStep<T> = (arg: T) => any;
 namespace BuildSystem {
   export type PreBuildStep = BuildStep<void>;
   export type PostBuildStep = BuildStep<void>;
-  export type PreObjectBuildStep = BuildStep<LObject>;
-  export type PostObjectBuildStep = BuildStep<LObject>;
-  export type AttributeBuildStep = BuildStep<Attribute>;
+  export type ObjectBuildStep = BuildStep<LObject>;
 }
 
 class BuildSystem {
@@ -17,12 +14,8 @@ class BuildSystem {
 
   public readonly preBuild: BuildSystem.PreBuildStep[] = [];
   public readonly postBuild: BuildSystem.PostBuildStep[] = [];
-  public readonly preObjectBuild:
-    { [type: string]: BuildSystem.PreObjectBuildStep[] } = {};
-  public readonly postObjectBuild:
-    { [type: string]: BuildSystem.PostObjectBuildStep[] } = {};
-  public readonly attributeBuild:
-    { [key: string]: BuildSystem.AttributeBuildStep[] } = {};
+  public readonly objectBuild:
+    { [type: string]: BuildSystem.ObjectBuildStep[] } = {};
 
   public addPreBuildStep(step: BuildSystem.PreBuildStep) {
     this.preBuild.push(step);
@@ -32,34 +25,14 @@ class BuildSystem {
     this.postBuild.push(step);
   }
 
-  public addPreObjectBuildStep(
-    step: BuildSystem.PreObjectBuildStep,
+  public addObjectBuildStep(
+    step: BuildSystem.ObjectBuildStep,
     type: string = '*'
   ) {
-    if (type in this.preObjectBuild)
-      this.preObjectBuild[type].push(step);
+    if (type in this.objectBuild)
+      this.objectBuild[type].push(step);
     else
-      this.preObjectBuild[type] = [step];
-  }
-
-  public addPostObjectBuildStep(
-    step: BuildSystem.PostObjectBuildStep,
-    type: string = '*'
-  ) {
-    if (type in this.postObjectBuild)
-      this.postObjectBuild[type].push(step);
-    else
-      this.postObjectBuild[type] = [step];
-  }
-
-  public addAttributeBuildStep(
-    step: BuildSystem.AttributeBuildStep,
-    key: string = '*'
-  ) {
-    if (key in this.attributeBuild)
-      this.attributeBuild[key].push(step);
-    else
-      this.attributeBuild[key] = [step];
+      this.objectBuild[type] = [step];
   }
 
   public build(project: Project) {
@@ -70,16 +43,8 @@ class BuildSystem {
     this.preBuild.forEach(b => b());
 
     for (let obj of project.objects.all()) {
-      this.runAll(this.preObjectBuild['*'], obj);
-      this.runAll(this.preObjectBuild[obj.type], obj);
-
-      for (let attr of obj.getAttributes()) {
-        this.runAll(this.attributeBuild['*'], attr);
-        this.runAll(this.attributeBuild[attr.key], attr);
-      }
-
-      this.runAll(this.postObjectBuild['*'], obj);
-      this.runAll(this.postObjectBuild[obj.type], obj);
+      this.runAll(this.objectBuild['*'], obj);
+      this.runAll(this.objectBuild[obj.type], obj);
     }
 
     this.postBuild.forEach(b => b());
