89404858e83f55a86a9bfb6c2b1a68483958fc3e
Kyle Cutler <kyle1320@gmail.com>
2019-07-21 19:56:52 -0400

    Button component example

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -6,7 +6,20 @@ window.addEventListener('load', function () {
   const project = new Project();
 
   const obj1 = project.makeObject('component');
-  obj1.addOwnField(BasicField.factory('html.attr.style', '')(project));
+  obj1.addOwnField('html.tag', new BasicField('button'));
+  obj1.addOwnField('html.innerContent', new BasicField('Click Me'));
+  obj1.addOwnField('html.attr.style', new BasicField('color: red'));
+
+  const obj2 = project.makeObject('component', obj1);
+  obj2.addOwnField('html.innerContent', new BasicField('Custom Text'));
+
+  const obj3 = project.makeObject('component', );
+  obj3.addOwnField(
+    'html.innerContent',
+    new BasicField(`The button is: {${obj2.id}|html.outerContent}`)
+  );
 
   document.body.appendChild(new ObjectEditor(obj1).element);
+  document.body.appendChild(new ObjectEditor(obj2).element);
+  document.body.appendChild(new ObjectEditor(obj3).element);
 });
