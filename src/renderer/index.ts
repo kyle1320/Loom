88f5d2c26bc0f61e8dc6f5dc5cf8642ce16bc723
Kyle Cutler <kyle1320@gmail.com>
2019-07-21 20:18:09 -0400

    Add onclick to example buttons

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -8,12 +8,14 @@ window.addEventListener('load', function () {
   const obj1 = project.makeObject('component');
   obj1.addOwnField('html.tag', new BasicField('button'));
   obj1.addOwnField('html.innerContent', new BasicField('Click Me'));
+  obj1.addOwnField('html.attr.onclick', new BasicField('alert(\'Base\')'));
   obj1.addOwnField('html.attr.style', new BasicField('color: red'));
 
   const obj2 = project.makeObject('component', obj1);
   obj2.addOwnField('html.innerContent', new BasicField('Custom Text'));
+  obj2.addOwnField('html.attr.onclick', new BasicField('alert(\'Custom\')'));
 
-  const obj3 = project.makeObject('component', );
+  const obj3 = project.makeObject('component');
   obj3.addOwnField(
     'html.innerContent',
     new BasicField(`The button is: {${obj2.id}|html.outerContent}`)
