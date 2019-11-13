1aaa254c6b36ecc3ae2f3e98f36ef654c31b70fd
Kyle Cutler <kyle1320@gmail.com>
2019-11-13 15:22:56 -0500

    Minor cleanup

--- src/common/events/FieldObserver.ts
+++ src/common/events/FieldObserver.ts
@@ -3,12 +3,8 @@ import EventEmitter from '../util/EventEmitter';
 // FieldObservers watch for changes to the value of a particular field.
 // These can either be recursive or not, and should emit an 'update' event
 // whenever the immediate or recursive content of a field changes.
-abstract class FieldObserver extends EventEmitter<{ update: void }> {
+export default abstract class FieldObserver
+  extends EventEmitter<{ update: void }> {
+
   public abstract destroy(): void;
 }
-
-namespace FieldObserver {
-  export type Callback = (type: string, data: Record<string, unknown>) => void;
-}
-
-export default FieldObserver;
--- src/common/extensions/DataExtension.ts
+++ src/common/extensions/DataExtension.ts
@@ -1,6 +1,5 @@
 import Project from '../data/Project';
 
-/* eslint-disable semi */
 export default interface DataExtension {
   initProject?(project: Project): void;
 }
