b589581213c6eaaa9a3e41c6a59bfccc31ec345d
Kyle Cutler <kyle1320@gmail.com>
2019-11-13 21:26:11 -0500

    Update "no object selected" view

--- src/renderer/LoomUI/Editor/index.tsx
+++ src/renderer/LoomUI/Editor/index.tsx
@@ -4,20 +4,26 @@ import DataObject from '../../../common/data/objects/DataObject';
 
 import './Editor.scss';
 import Properties from './Properties';
+import ObjectEditor from '../../registry/ObjectEditor';
 
 interface Props { context: DataObject | null }
+
+const NoObjectEditor: ObjectEditor = () => {
+  return <div className="no-object-editor">
+    <div className="heading">No Object Selected</div>
+    <div className="subtitle">Select an object to view / edit</div>
+  </div>;
+}
 
 const Editor: React.FC<Props> = (props: Props) => {
   const workspace = useWorkspace();
   const registry = workspace.registry;
   const ObjectEditor = props.context
-    && registry.getObjectEditor(props.context)
-    || Properties;
+    ? registry.getObjectEditor(props.context) || Properties
+    : NoObjectEditor;
 
   return <div className="editor">
-    { props.context
-      ? <ObjectEditor object={props.context!} />
-      : 'no object selected' }
+    <ObjectEditor object={props.context!} />
   </div>;
 }
 
