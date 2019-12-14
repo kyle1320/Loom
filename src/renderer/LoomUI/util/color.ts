37d9d3df454f1cd38b3831c3b8cbdc3c3d020b84
Kyle Cutler <kyle1320@gmail.com>
2019-12-14 11:47:46 -0500

    Switch to light theme

--- src/renderer/LoomUI/util/color.ts
+++ src/renderer/LoomUI/util/color.ts
@@ -1,9 +1,19 @@
-export const primary0 = '#303040';
-export const primary1 = '#414157';
-export const primary2 = '#52526e';
-export const primary3 = '#636385';
+// export const primary0 = '#303040';
+// export const primary1 = '#414157';
+// export const primary2 = '#52526e';
+// export const primary3 = '#636385';
 
-export const primaryhl0 = '#1c1c40';
-export const primaryhl1 = '#252557';
-export const primaryhl2 = '#2f2f6e';
-export const primaryhl3 = '#393985';
+// export const primaryhl0 = '#1c1c40';
+// export const primaryhl1 = '#252557';
+// export const primaryhl2 = '#2f2f6e';
+// export const primaryhl3 = '#393985';
+
+export const primary0 = '#ffffff';
+export const primary1 = '#f5f5f5';
+export const primary2 = '#ebebeb';
+export const primary3 = '#e0e0e0';
+
+export const primaryhl0 = '#d8d8ff';
+export const primaryhl1 = '#cfcff5';
+export const primaryhl2 = '#c7c7eb';
+export const primaryhl3 = '#bebee0';
