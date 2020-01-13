084285f36308d64f4c43a01aca5c6233bb20ac05
Kyle Cutler <kyle1320@gmail.com>
2020-01-13 17:46:22 -0500

    Remove Toolbar (for now)

--- src/renderer/LoomUI/index.tsx
+++ src/renderer/LoomUI/index.tsx
@@ -2,12 +2,10 @@ import React, { useState } from 'react';
 import SplitPane from 'react-split-pane';
 
 import Editor from './Editor';
-import Toolbar from './Toolbar';
 import Navigator from './Navigator';
 import WorkspaceContext from './WorkspaceContext';
 import Workspace from '../Workspace';
 import DataObject from '../../common/data/objects/DataObject';
-import UIContainer from './util/UIContainer';
 import * as color from './util/color';
 
 import './LoomUI.scss';
@@ -18,18 +16,14 @@ const LoomUI: React.FC<Props> = (props: Props) => {
   const [selected, select] = useState<DataObject | null>(null);
 
   return <WorkspaceContext.Provider value={props.workspace}>
-    <UIContainer flow="n" size={30} color={color.primary3}
-      first={<Toolbar context={selected} />}
-      second={
-        <SplitPane
-          split="vertical"
-          minSize={100}
-          defaultSize={200}
-          pane1Style={{ backgroundColor: color.primary1 }} >
-          <Navigator context={selected} onSelect={select} />
-          <Editor context={selected} />
-        </SplitPane>
-      } />
+    <SplitPane
+      split="vertical"
+      minSize={100}
+      defaultSize={200}
+      pane1Style={{ backgroundColor: color.primary1 }} >
+      <Navigator context={selected} onSelect={select} />
+      <Editor context={selected} />
+    </SplitPane>
   </WorkspaceContext.Provider>;
 }
 
