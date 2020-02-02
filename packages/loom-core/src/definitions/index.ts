ae4850e5ffb85c949dd045753f49a7c6dd6cd606
Kyle Cutler <kyle1320@gmail.com>
2020-02-02 16:53:28 -0500

    Add pages, stylesheets to Sources

--- packages/loom-core/src/definitions/index.ts
+++ packages/loom-core/src/definitions/index.ts
@@ -1,6 +1,8 @@
-import { ElementDef } from './HTML';
+import { ElementDef, PageDef } from './HTML';
 import { BuildResult } from '../build';
 import { WritableStringMap } from '../data/StringMap';
+import { WritableList } from '../data/List';
+import { SheetDef } from './CSS';
 
 export interface Definition {
   build(sources: Sources): BuildResult<Definition>;
@@ -10,13 +12,22 @@ export class Sources {
   public readonly vars: WritableStringMap<string>;
   public readonly components: WritableStringMap<ElementDef>;
 
+  public readonly pages: WritableList<PageDef>;
+  public readonly stylesheets: WritableList<SheetDef>;
+
   public constructor(
     vars: WritableStringMap<string> | Record<string, string> = {},
-    components: WritableStringMap<ElementDef> | Record<string, ElementDef> = {}
+    components: WritableStringMap<ElementDef> | Record<string, ElementDef> = {},
+    pages: WritableList<PageDef> | PageDef[] = [],
+    stylesheets: WritableList<SheetDef> | SheetDef[] = []
   ) {
     this.vars = vars instanceof WritableStringMap
       ? vars : new WritableStringMap(vars);
     this.components = components instanceof WritableStringMap
       ? components : new WritableStringMap(components);
+    this.pages = pages instanceof WritableList
+      ? pages : new WritableList(pages)
+    this.stylesheets = stylesheets instanceof WritableList
+      ? stylesheets : new WritableList(stylesheets);
   }
 }
