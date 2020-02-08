6dd4fe03df6f2abae84763ef1f7ae39f62fa1253
Kyle Cutler <kyle1320@gmail.com>
2020-02-07 23:26:20 -0500

    Export everything from loom-core

--- packages/loom-core/src/index.ts
+++ packages/loom-core/src/index.ts
@@ -1,3 +1,12 @@
-export default function test(): string {
-  return 'Hello World!';
-}
+export * from './definitions';
+export * from './definitions/CSS';
+export * from './definitions/HTML';
+
+export * from './data/List';
+export * from './data/StringMap';
+
+export * from './build';
+export * from './build/CSS';
+export * from './build/HTML';
+
+export { EventEmitter } from './util/EventEmitter';
