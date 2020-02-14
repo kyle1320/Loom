ac8a32b4d17acf14afea2e7ef44bebafbdb6f6ad
Kyle Cutler <kyle1320@gmail.com>
2020-02-14 16:43:44 -0500

    Add icons to NodeNavigator

--- packages/loom-ui/src/renderer/LoomUI/Navigator/NodeNavigator.ts
+++ packages/loom-ui/src/renderer/LoomUI/Navigator/NodeNavigator.ts
@@ -8,7 +8,7 @@ import './NodeNavigator.scss';
 
 type Node = loom.Element | loom.TextNode | loom.Component;
 
-export default class NodeNavigator extends UIComponent {
+export default class NodeNavigator extends UIComponent<{}, HTMLElement> {
   public constructor(
     ui: LoomUI,
     node: Node,
@@ -22,25 +22,36 @@ export default class NodeNavigator extends UIComponent {
     } else if (node instanceof loom.TextNode) {
       this.appendChild(new TextNodeNavigator(ui, node, depth));
     } else if (node instanceof loom.Component) {
+      this.el.className += ' node-nav__container--component'
       this.appendChild(new ComponentNavigator(ui, node, depth));
       this.appendChild(new ComponentChildrenNavigator(ui, node, depth));
     }
   }
 }
 
-class SingleNodeNavigator extends UIComponent<{}, HTMLElement> {
+abstract class SingleNodeNavigator<N extends Node = Node>
+  extends UIComponent<{}, HTMLElement> {
+
+  private readonly iconEl: HTMLElement;
+  private readonly titleEl: HTMLElement;
+
   public constructor(
     ui: LoomUI,
-    private readonly node: Node,
+    protected readonly node: N,
     type: string,
     title: string,
     depth = 1
   ) {
     super(makeElement('div', {
       className: 'node-nav node-nav__' + type,
-      style: { paddingLeft: (depth*10) + 'px' },
+      style: { paddingLeft: (depth*10 + 15) + 'px' },
       onclick: () => ui.selectData(node)
-    }, title));
+    }));
+
+    this.iconEl = makeElement('i', { className: this.getIcon() });
+    this.el.appendChild(this.iconEl);
+    this.titleEl = makeElement('div', { className: 'node-nav__title' }, title);
+    this.el.appendChild(this.titleEl);
 
     ui.on('updateData', this.updateSelected)
     this.updateSelected(ui.getSelectedData());
@@ -49,9 +60,19 @@ class SingleNodeNavigator extends UIComponent<{}, HTMLElement> {
   private updateSelected = (data: DataTypes | null): void => {
     toggleClass(this.el, 'selected', data === this.node);
   }
+
+  protected setTitle = (title: string): void => {
+    this.titleEl.textContent = title;
+  }
+
+  protected abstract getIcon(): string;
+
+  protected setIcon(icon: string | null = null): void {
+    this.iconEl.className = icon || this.getIcon();
+  }
 }
 
-class TextNodeNavigator extends SingleNodeNavigator {
+class TextNodeNavigator extends SingleNodeNavigator<loom.TextNode> {
   public constructor(
     ui: LoomUI,
     node: loom.TextNode,
@@ -59,11 +80,15 @@ class TextNodeNavigator extends SingleNodeNavigator {
   ) {
     super(ui, node, 'text', node.content, depth);
 
-    this.listen(node, 'contentChanged', val => this.el.textContent = val);
+    this.listen(node, 'contentChanged', this.setTitle);
+  }
+
+  protected getIcon(): string {
+    return 'fa fa-quote-right';
   }
 }
 
-class ElementNavigator extends SingleNodeNavigator {
+class ElementNavigator extends SingleNodeNavigator<loom.Element> {
   public constructor(
     ui: LoomUI,
     node: loom.Element,
@@ -71,7 +96,37 @@ class ElementNavigator extends SingleNodeNavigator {
   ) {
     super(ui, node, 'element', node.tag, depth);
 
-    this.listen(node, 'tagChanged', val => this.el.textContent = val);
+    this.listen(node, 'tagChanged', this.tagChanged);
+  }
+
+  protected getIcon(): string {
+    switch (this.node.tag) {
+      case '': return 'fa fa-exclamation-triangle';
+      case 'a': return 'fa fa-link';
+      case 'b': return 'fa fa-bold';
+      case 'i': return 'fa fa-italic';
+      case 's': return 'fa fa-strikethrough';
+      case 'p': return 'fa fa-paragraph';
+      case 'u': return 'fa fa-underline';
+      case 'h1':
+      case 'h2':
+      case 'h3':
+      case 'h4':
+      case 'h5':
+      case 'h6': return 'fa fa-heading';
+      case 'hr': return 'fa fa-horizontal-rule';
+      case 'ol': return 'fa fa-list-ol';
+      case 'ul': return 'fa fa-list-ul';
+      case 'sub': return 'fa fa-subscript';
+      case 'sup': return 'fa fa-superscript';
+      case 'table': return 'fa fa-table';
+    }
+    return 'fa fa-code';
+  }
+
+  protected tagChanged = (tag: string): void => {
+    this.setIcon();
+    this.setTitle(tag);
   }
 }
 
@@ -94,15 +149,25 @@ class ElementChildrenNavigator extends UIComponent {
   }
 }
 
-class ComponentNavigator extends SingleNodeNavigator {
+class ComponentNavigator extends SingleNodeNavigator<loom.Component> {
   public constructor(
     ui: LoomUI,
     node: loom.Component,
     depth = 1
   ) {
-    super(ui, node, 'element', node.source.name, depth);
+    super(ui, node, 'component', node.source.name, depth);
 
-    this.listen(node.source, 'nameChanged', val => this.el.textContent = val);
+    this.listen(node.source, 'nameChanged', this.nameChanged);
+  }
+
+  protected getIcon(): string {
+    return this.node.element instanceof loom.EmptyComponent
+      ? 'fa fa-question' : 'fa fa-clone';
+  }
+
+  protected nameChanged = (name: string): void => {
+    this.setIcon();
+    this.setTitle(name);
   }
 }
 
