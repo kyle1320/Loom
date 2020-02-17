eb5b73c800f8f25e444953bdb6eead8704a3392d
Kyle Cutler <kyle1320@gmail.com>
2020-02-17 18:00:03 -0500

    Add tabs to Properties bar, initial Style editors

--- packages/loom-ui/src/renderer/LoomUI/Editor/ElementEditor/PropertiesEditor/index.ts
+++ packages/loom-ui/src/renderer/LoomUI/Editor/ElementEditor/PropertiesEditor/index.ts
@@ -9,23 +9,155 @@ import ValueField from '../../../common/ValueField';
 
 import './PropertiesEditor.scss';
 
+type TabName = 'page' | 'element' | 'style';
+
+function iconForTab(tab: TabName): string {
+  switch (tab) {
+    case 'page': return 'fa fa-file';
+    case 'element': return 'fa fa-code';
+    case 'style': return 'fa fa-brush';
+  }
+}
+
+class PropertyTab extends UIComponent<{
+  'select': void;
+}, HTMLElement> {
+  public constructor(
+    public readonly name: TabName,
+    private selected: boolean
+  ) {
+    super(makeElement('div', {
+      className: 'property-tab ' + iconForTab(name),
+      onclick: () => this.select()
+    }));
+
+    this.select(selected);
+  }
+
+  public select(selected = true): void {
+    this.el.classList.toggle('selected', selected);
+    const wasSelected = this.selected;
+    this.selected = selected;
+    if (selected && selected !== wasSelected) {
+      this.emit('select');
+    }
+  }
+}
+
+class PropertyTabHeader extends UIComponent<{
+  'select': [TabName];
+}> {
+  private tabs: PropertyTab[] = [];
+
+  public constructor() {
+    super(makeElement('div', { className: 'properties-editor__header' }));
+  }
+
+  public update(tabs: TabName[], selected: TabName): void {
+    this.empty();
+    this.tabs = [];
+
+    for (const name of tabs) {
+      const tab = new PropertyTab(name, name === selected)
+        .on('select', () => this.select(name));
+      this.tabs.push(tab);
+      this.appendChild(tab);
+    }
+  }
+
+  private select = (name: TabName): void => {
+    this.tabs.forEach(t => t.select(t.name === name));
+    this.emit('select', name);
+  }
+}
+
+class PropertyContents extends UIComponent {
+  private selectedTab: TabName = 'page';
+
+  public constructor(private readonly ui: LoomUI) {
+    super(makeElement('div', { className: 'properties-editor__content' }));
+
+    this.autoCleanup(ui.onOff('updateData', this.update));
+    this.autoCleanup(ui.onOff('updateContent', this.update));
+
+    this.update();
+  }
+
+  public select(name: TabName): void {
+    this.selectedTab = name;
+    this.update();
+  }
+
+  public update = (): void => {
+    this.empty();
+
+    const data = this.ui.getSelectedData();
+    // const content = this.ui.getSelectedContent();
+
+    switch (this.selectedTab) {
+      case 'page':
+        break;
+      case 'element':
+        if (data instanceof loom.TextNode) {
+          this.appendChild(new ValueField('Content', data.source.content));
+        } else if (data instanceof loom.Element) {
+          this.appendChild(new ValueField('Tag', data.source.tag));
+          this.appendChild(new ValueField('Id',
+            new LookupValue(data.source.attrs, 'id')));
+          this.appendChild(new KeyValueList('Attributes', data.source.attrs));
+        } else if (data instanceof loom.Component) {
+          this.appendChild(new ValueField('Name', data.source.name));
+        }
+        break;
+      case 'style':
+        for (const rule of this.ui.globalStyles.rules) {
+          this.appendChild(
+            new KeyValueList(rule.selector.get(), rule.style.source));
+        }
+        break;
+    }
+  }
+}
+
 export default class PropertiesEditor extends UIComponent {
-  public constructor(ui: LoomUI) {
+  private selectedTab!: TabName;
+  private tabHeader: PropertyTabHeader;
+  private contents: PropertyContents;
+
+  public constructor(private readonly ui: LoomUI) {
     super(makeElement('div', { className: 'properties-editor' }));
 
-    this.autoCleanup(ui.onOff('updateData', data => {
-      this.empty();
+    this.appendChild(this.tabHeader = new PropertyTabHeader()
+      .on('select', name => this.contents.select(name)));
+    this.appendChild(this.contents = new PropertyContents(ui));
 
-      if (data instanceof loom.TextNode) {
-        this.appendChild(new ValueField('Content', data.source.content));
-      } else if (data instanceof loom.Element) {
-        this.appendChild(new ValueField('Tag', data.source.tag));
-        this.appendChild(new ValueField('Id',
-          new LookupValue(data.source.attrs, 'id')));
-        this.appendChild(new KeyValueList('Attributes', data.source.attrs));
-      } else if (data instanceof loom.Component) {
-        this.appendChild(new ValueField('Name', data.source.name));
-      }
-    }));
+    this.autoCleanup(ui.onOff('updateData', this.build));
+    this.autoCleanup(ui.onOff('updateContent', this.build));
+
+    this.build();
+  }
+
+  private build = (): void => {
+    const content = this.ui.getSelectedContent();
+    const data = this.ui.getSelectedData();
+    const allTabs: TabName[] = [];
+
+    if (content instanceof loom.Page) {
+      allTabs.push('page');
+    }
+
+    if (content instanceof loom.Element ||
+        data instanceof loom.TextNode ||
+        data instanceof loom.Element ||
+        data instanceof loom.Component) {
+      allTabs.push('element');
+    }
+
+    allTabs.push('style');
+
+    if (allTabs.indexOf(this.selectedTab) < 0) {
+      this.selectedTab = allTabs[0] || 'page';
+    }
+    this.tabHeader.update(allTabs, this.selectedTab);
   }
 }
