79bed708383cdf6d99288612baf3ead33f6b5baf
Kyle Cutler <kyle1320@gmail.com>
2020-02-16 19:39:14 -0500

    Style changes & bugfixes for KeyValueList

--- packages/loom-ui/src/renderer/LoomUI/common/KeyValueList.ts
+++ packages/loom-ui/src/renderer/LoomUI/common/KeyValueList.ts
@@ -1,8 +1,7 @@
 import * as loom from 'loom-core';
 
-import IconButton from './IconButton';
 import { UIComponent } from '../UIComponent';
-import { makeElement } from '../util/dom';
+import { makeElement, toggleClass } from '../util/dom';
 
 import './KeyValueList.scss';
 
@@ -10,13 +9,12 @@ class KeyValueListHeader extends UIComponent<{ remove: void }> {
   public constructor(title: string) {
     super(
       makeElement('div', { className: 'keyvaluelist__header' },
-        makeElement('div', { className: 'keyvaluelist__title' }, title)),
-      new IconButton('fa-minus').on('click', () => this.emit('remove'))
+        makeElement('div', { className: 'keyvaluelist__title' }, title))
     );
   }
 }
 
-class KeyValueListContent extends UIComponent<{ select: string | null }> {
+class KeyValueListContent extends UIComponent {
   private newRow: KeyValueListRow;
   private rows: Record<string, KeyValueListRow> = {};
 
@@ -25,9 +23,10 @@ class KeyValueListContent extends UIComponent<{ select: string | null }> {
   ) {
     super(makeElement('div', { className: 'keyvaluelist__content' }));
 
-    this.appendChild(this.newRow = new KeyValueListRow('', '')
-      .on('changeKey', this.addNewRow)
-    )
+    this.appendChild(
+      this.newRow = new KeyValueListRow('', '').on('changeKey', this.addNewRow)
+    );
+    this.newRow.isNewRow();
 
     for (const name of data.keys()) {
       this.setRow(name, data.get(name)!);
@@ -37,37 +36,40 @@ class KeyValueListContent extends UIComponent<{ select: string | null }> {
     this.listen(data, 'delete', key => this.deleteRow(key));
   }
 
-  private addNewRow = (key: string): void => {
+  private addNewRow = (): void => {
+    const key = this.data.normalizeKey(this.newRow.getKey());
     if (this.data.has(key)) {
       this.newRow.setKey('');
+      const row = this.rows[key];
+      row && row.selectValue();
       return;
     }
 
     const row = this.newRow;
-    key = this.data.normalizeKey(key);
     row.setKey(key);
-    this.rows[key] = this.listenRow(
-      row.off('changeKey', this.addNewRow), key);
+    this.rows[key] = this.listenRow(row.off('changeKey', this.addNewRow));
     this.data.set(key, row.getValue());
     this.appendChild(
-      this.newRow = new KeyValueListRow('', '')
-        .on('changeKey', this.addNewRow)
+      this.newRow = new KeyValueListRow('', '').on('changeKey', this.addNewRow)
     );
+    this.newRow.isNewRow();
   }
 
-  private listenRow(row: KeyValueListRow, key: string): KeyValueListRow {
+  private listenRow(row: KeyValueListRow): KeyValueListRow {
+    row.isNewRow(false);
     return row
-      .on('changeValue', value => row.setKey(this.data.set(key, value)))
-      .on('changeKey', newKey => this.changeKey(row, key, newKey))
-      .on('select', () => this.emit('select', key))
-      .on('deselect', () => this.emit('select', null));
+      .on('changeValue',
+        ({ key, value }) => row.setKey(this.data.set(key, value)))
+      .on('changeKey',
+        ({ oldKey, newKey }) => this.changeKey(row, oldKey, newKey))
+      .on('delete', key => this.deleteRow(key));
   }
 
   private setRow(key: string, value: string): void {
     if (key in this.rows) {
       this.rows[key].setValue(value);
     } else {
-      const row = this.listenRow(new KeyValueListRow(key, value), key);
+      const row = this.listenRow(new KeyValueListRow(key, value));
       this.rows[key] = row;
       this.insertChild(row, this.children.length - 1);
     }
@@ -76,7 +78,8 @@ class KeyValueListContent extends UIComponent<{ select: string | null }> {
   private deleteRow(key: string): void {
     const row = this.rows[key];
     delete this.rows[key];
-    row.destroy();
+    this.data.delete(key);
+    row && row.destroy();
   }
 
   private changeKey(
@@ -84,49 +87,74 @@ class KeyValueListContent extends UIComponent<{ select: string | null }> {
     oldKey: string,
     newKey: string
   ): void {
+    newKey = this.data.normalizeKey(newKey);
     if (this.data.has(newKey)) {
       row.setKey(oldKey);
       return;
     }
     const value = this.data.get(oldKey) || '';
+    delete this.rows[oldKey];
     this.data.delete(oldKey);
-    if (newKey) row.setKey(this.data.set(newKey, value));
+    if (newKey) {
+      this.rows[newKey] = row;
+      row.setKey(this.data.set(newKey, value));
+    } else {
+      row.destroy();
+    }
   }
 }
 
 class KeyValueListRow extends UIComponent<{
-  select: void;
-  deselect: void;
-  changeValue: string;
-  changeKey: string;
+  changeValue: { key: string; value: string };
+  changeKey: { oldKey: string; newKey: string };
+  delete: string;
 }, HTMLElement> {
+  private readonly removeBtn: HTMLElement;
   private readonly keyInput: HTMLInputElement;
   private readonly valueInput: HTMLInputElement;
 
+  private key: string;
+
   public constructor(key: string, value: string) {
     super(makeElement('div', { className: 'keyvaluelist__row' }));
+
+    this.key = key;
+
+    this.el.appendChild(this.removeBtn = makeElement('div', {
+      className: 'icon-btn fa fa-minus',
+      onclick: () => this.emit('delete', this.getKey())
+    }));
 
     this.el.appendChild(this.keyInput = makeElement('input', {
       className: 'keyvaluelist__key-input',
       value: key,
       placeholder: 'Key',
-      onchange: () => this.emit('changeKey', this.keyInput.value),
-      onfocus: () => this.emit('select'),
-      onblur: () => this.emit('deselect')
+      oninput: () => this.enableValueInput(),
+      onchange: () => {
+        const oldKey = this.key;
+        this.key = this.getKey();
+        this.emit('changeKey', { oldKey, newKey: this.key });
+      }
     }));
 
     this.el.appendChild(this.valueInput = makeElement('input', {
       className: 'keyvaluelist__value-input',
       value: value,
       placeholder: 'Value',
-      oninput: () => this.emit('changeValue', this.valueInput.value),
-      onfocus: () => this.emit('select'),
-      onblur: () => this.emit('deselect')
+      oninput: () => this.emit('changeValue', {
+        key: this.getKey(),
+        value: this.getValue()
+      })
     }));
   }
 
   public setKey(value: string): void {
-    this.keyInput.value = value;
+    this.key = this.keyInput.value = value;
+    this.enableValueInput();
+  }
+
+  public getKey(): string {
+    return this.keyInput.value;
   }
 
   public setValue(value: string): void {
@@ -136,17 +164,27 @@ class KeyValueListRow extends UIComponent<{
   public getValue(): string {
     return this.valueInput.value;
   }
+
+  public selectValue(): void {
+    this.valueInput.focus();
+  }
+
+  public isNewRow(isNewRow = true): void {
+    toggleClass(this.removeBtn, 'disabled', isNewRow);
+    this.enableValueInput(isNewRow || undefined);
+  }
+
+  private enableValueInput(disable = !this.getKey()): void {
+    toggleClass(this.valueInput, 'disabled', disable);
+    this.valueInput.disabled = disable;
+  }
 }
 
 export default class KeyValueList extends UIComponent {
-  private selected: string | null = null;
-
   public constructor(title: string, data: loom.WritableStringMap<string>) {
     super(makeElement('div', { className: 'keyvaluelist' }),
-      new KeyValueListHeader(title)
-        .on('remove', () => this.selected && data.delete(this.selected)),
+      new KeyValueListHeader(title),
       new KeyValueListContent(data)
-        .on('select', key => this.selected = key)
     );
   }
 }
