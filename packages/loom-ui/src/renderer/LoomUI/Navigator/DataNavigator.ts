d56faf831f69ae8cc32d0f32220c417336b88eaa
Kyle Cutler <kyle1320@gmail.com>
2020-02-18 12:33:59 -0500

    Make navigation a 2-step process

--- packages/loom-ui/src/renderer/LoomUI/Navigator/DataNavigator.ts
+++ packages/loom-ui/src/renderer/LoomUI/Navigator/DataNavigator.ts
@@ -4,14 +4,22 @@ import NodeNavigator from './NodeNavigator';
 import LoomUI from '..';
 import { makeElement } from '../util/dom';
 import { UIComponent } from '../UIComponent';
+import IconButton from '../common/IconButton';
 
 import './DataNavigator.scss';
 
-export default class DataNavigator extends UIComponent {
-  public constructor(ui: LoomUI) {
-    super(makeElement('div', { className: 'data-nav' }));
+class DataNavigatorHeader extends UIComponent<{ back: void }> {
+  public constructor() {
+    super(makeElement('div', { className: 'data-nav__header' }),
+      new IconButton('fa fa-arrow-left').on('click', () => this.emit('back')));
+  }
+}
 
-    this.autoCleanup(ui.onOff('updateContent', content => {
+class DataNavigatorContent extends UIComponent {
+  public constructor(ui: LoomUI) {
+    super(makeElement('div', { className: 'data-nav__content' }));
+
+    this.autoCleanup(ui.content.watch(content => {
       this.empty();
 
       if (content instanceof loom.Element) {
@@ -21,5 +29,13 @@ export default class DataNavigator extends UIComponent {
         this.appendChild(new NodeNavigator(ui, content.body));
       }
     }));
+  }
+}
+
+export default class DataNavigator extends UIComponent<{ back: void }> {
+  public constructor(ui: LoomUI) {
+    super(makeElement('div', { className: 'data-nav' }),
+      new DataNavigatorHeader().on('back', () => this.emit('back')),
+      new DataNavigatorContent(ui));
   }
 }
--- packages/loom-ui/src/renderer/LoomUI/Navigator/index.ts
+++ packages/loom-ui/src/renderer/LoomUI/Navigator/index.ts
@@ -7,10 +7,26 @@ import { UIComponent } from '../UIComponent';
 import './Navigator.scss';
 
 export default class Navigator extends UIComponent {
-  public constructor(ui: LoomUI) {
-    super(makeElement('div', { className: 'navigator' }),
-      new DefinitionNavigator(ui),
-      new DataNavigator(ui)
-    );
+  private showingData!: boolean;
+
+  public constructor(
+    private readonly ui: LoomUI
+  ) {
+    super(makeElement('div', { className: 'navigator' }));
+
+    this.autoCleanup(ui.content.watch(content => this.update(!!content)));
+  }
+
+  private update = (showingData: boolean): void => {
+    if (this.showingData !== showingData) {
+      this.showingData = showingData;
+
+      this.empty();
+
+      this.appendChild(this.showingData
+        ? new DataNavigator(this.ui)
+          .on('back', () => this.ui.contentDef.set(null))
+        : new DefinitionNavigator(this.ui));
+    }
   }
 }
