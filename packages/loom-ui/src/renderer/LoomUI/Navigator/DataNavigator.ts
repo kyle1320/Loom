b1d9ac164387e7b3a90abe32d927291ce4823b63
Kyle Cutler <kyle1320@gmail.com>
2020-02-18 20:58:29 -0500

    Display current location in DataNavigator

--- packages/loom-ui/src/renderer/LoomUI/Navigator/DataNavigator.ts
+++ packages/loom-ui/src/renderer/LoomUI/Navigator/DataNavigator.ts
@@ -9,9 +9,32 @@ import IconButton from '../common/IconButton';
 import './DataNavigator.scss';
 
 class DataNavigatorHeader extends UIComponent<{ back: void }> {
-  public constructor() {
+  private locationEl: HTMLElement;
+
+  public constructor(ui: LoomUI) {
     super(makeElement('div', { className: 'data-nav__header' }),
       new IconButton('fa fa-arrow-left').on('click', () => this.emit('back')));
+
+    this.appendChild(new UIComponent(
+      this.locationEl = makeElement('div', { className: 'data-nav__location' })
+    ));
+
+    let cleanup: (() => void) | null = null;
+    this.autoCleanup(ui.contentDef.watch(key => {
+      cleanup && cleanup();
+      cleanup = key && key.watch(this.setTitle.bind(this, key.value.get()));
+    }), () => cleanup && cleanup());
+  }
+
+  private setTitle = (
+    value: loom.PageDef | loom.ElementDef | undefined,
+    key: string
+  ): void => {
+    if (value instanceof loom.ElementDef) {
+      this.locationEl.textContent = 'Component: ' + key;
+    } else {
+      this.locationEl.textContent = 'Page: ' + key;
+    }
   }
 }
 
@@ -35,7 +58,7 @@ class DataNavigatorContent extends UIComponent {
 export default class DataNavigator extends UIComponent<{ back: void }> {
   public constructor(ui: LoomUI) {
     super(makeElement('div', { className: 'data-nav' }),
-      new DataNavigatorHeader().on('back', () => this.emit('back')),
+      new DataNavigatorHeader(ui).on('back', () => this.emit('back')),
       new DataNavigatorContent(ui));
   }
 }
