56cfa56a850b975c7cadb4333f60d35b9155d4c5
Kyle Cutler <kyle1320@gmail.com>
2020-03-11 21:23:04 -0400

    Export Destroyable interface / namespace

--- packages/loom-data/src/index.ts
+++ packages/loom-data/src/index.ts
@@ -1,4 +1,8 @@
+import Destroyable from './types/Destroyable';
+
 export * from './types/EventEmitter';
 export * from './types/List';
 export * from './types/Dictionary';
 export * from './types/Value';
+
+export { Destroyable };
