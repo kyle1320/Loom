cd9170f94949397ac1edb478ee292d2a294e0807
Kyle Cutler <kyle1320@gmail.com>
2020-03-23 17:08:06 -0400

    Support disabling buttons, content parsing

--- packages/loom-ui/src/renderer/common/Button.ts
+++ packages/loom-ui/src/renderer/common/Button.ts
@@ -1,11 +1,11 @@
 import { UIComponent } from '@/UIComponent';
-import { makeElement } from '@/util/dom';
+import { makeElement, toggleClass, parseContents } from '@/util/dom';
 
 import './Button.scss';
 
 type ButtonStyle = 'primary' | 'large';
-export default class Button extends UIComponent<{ click: void }> {
-  public constructor(text: string, ...styles: ButtonStyle[]) {
+export default class Button extends UIComponent<{ click: void }, HTMLElement> {
+  public constructor(contents: string, ...styles: ButtonStyle[]) {
     super(makeElement('div', {
       className: 'btn' + styles.map(x => ' btn--' + x).join(''),
       onclick: e => {
@@ -14,6 +14,10 @@ export default class Button extends UIComponent<{ click: void }> {
       },
       onkeydown: e => e.keyCode == 13 && this.emit('click'),
       tabIndex: 0
-    }, text));
+    }, parseContents(contents)));
+  }
+
+  public disable(disabled = true): void {
+    toggleClass(this.el, 'disabled', disabled);
   }
 }
