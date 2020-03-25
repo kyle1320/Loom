f3a67094460a04d58fc91b947cc04fc53ab865d1
Kyle Cutler <kyle1320@gmail.com>
2020-03-25 11:57:47 -0400

    Add focus & blur events to IconButton

--- packages/loom-ui/src/renderer/common/IconButton.ts
+++ packages/loom-ui/src/renderer/common/IconButton.ts
@@ -3,14 +3,21 @@ import { makeElement } from '@/util/dom';
 
 import './IconButton.scss';
 
-export default class IconButton extends UIComponent<{ click: void }> {
+export default class IconButton extends UIComponent<{
+  click: void;
+  focus: void;
+  blur: void;
+}> {
   public constructor(icon: string) {
     super(makeElement('div', {
       className: 'icon-btn fa ' + icon,
       onclick: e => {
+        e.preventDefault();
         e.stopPropagation();
         this.emit('click');
       },
+      onfocus: () => this.emit('focus'),
+      onblur: () => this.emit('blur'),
       onkeydown: e => e.keyCode == 13 && this.emit('click'),
       tabIndex: 0
     }));
