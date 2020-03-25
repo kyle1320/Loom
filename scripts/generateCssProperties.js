e41ac841034687f60a033504e78c87cb880c077b
Kyle Cutler <kyle1320@gmail.com>
2020-03-25 14:12:39 -0400

    Add script for generating CSS properties file

---
+++ scripts/generateCssProperties.js
@@ -0,0 +1,64 @@
+const fetch = require('node-fetch');
+
+(async function () {
+  const properties = await fetch(
+    'https://github.com/mdn/data/raw/master/css/properties.json'
+  ).then(res => res.ok && res.json());
+  const syntaxes = await fetch(
+    'https://github.com/mdn/data/raw/master/css/properties.json'
+  ).then(res => res.ok && res.json());
+
+  if (!properties) return console.error('failed to fetch properties.json');
+  if (!syntaxes) return console.error('failed to fetch syntaxes.json');
+
+  function resolveSyntax(syntax) {
+    const syntaxParts = syntax.split(' | ');
+    if (syntaxParts.length === 1) {
+      if (syntax.match(/^<[a-z-]+>$/i)) {
+        const name = syntax.substring(1, syntax.length - 1);
+        if (name in syntaxes) return resolveSyntax(syntaxes[name].syntax);
+      }
+      if (syntax.match(/^<'[a-z-]+'>$/i)) {
+        const data = properties[syntax.substring(2, syntax.length - 2)];
+        if (data) data.syntax;
+      }
+      if (syntax === '<string>' || syntax === '<string>+') return '';
+      return syntax;
+    }
+    return syntaxParts.map(resolveSyntax).filter(Boolean).join(' | ');
+  }
+
+  function getType(syntax) {
+    if (syntax && syntax.match(/^([a-z-]+( \| ))*[a-z-]*$/i)) return 'select';
+    if (syntax === '<color>') return 'color';
+    if (syntax === '<number>') return 'number';
+    return 'any';
+  }
+
+  const all = {};
+
+  for (const key in properties) {
+    if (key.startsWith('-')) continue;
+
+    const data = properties[key];
+    const syntax = resolveSyntax(data.syntax)
+    const info = { type: getType(syntax) };
+
+    if (syntax.match(/^([a-z-]+( \| ))*[a-z-]*$/i)) {
+      info.values = syntax.split(' | ').concat('initial', 'inherit')
+    }
+
+    await fetch(`https://developer.mozilla.org/en-US/docs/Web/CSS/${key}$json`)
+      .then(res => res.ok && res.json())
+      .then(json => {
+        info.summary = json
+          ? json.summary.replace(/<a.*?>(.*?)<\/a>/g, '$1')
+          : ''
+      });
+
+    all[key] = info;
+  }
+
+  console.log('/* eslint-disable */');
+  console.log('export default ' + JSON.stringify(all, null, 2) + ' as const');
+}());
