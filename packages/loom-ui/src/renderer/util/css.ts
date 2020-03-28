c053b2fc63c2383fe4e71b448863d195584c9cc0
Kyle Cutler <kyle1320@gmail.com>
2020-03-28 19:39:17 -0400

    Create InlineStyleRuleDef helper class

--- packages/loom-ui/src/renderer/util/css.ts
+++ packages/loom-ui/src/renderer/util/css.ts
@@ -1,4 +1,5 @@
 import * as loom from 'loom-core';
+import { Destroyable } from 'loom-data/src';
 
 function many(...callbacks: (() => void)[]): () => void {
   return () => callbacks.forEach(cb => cb());
@@ -55,4 +56,42 @@ function attachStyle(
     },
     delete: key => cssStyle.removeProperty(key)
   });
+}
+
+export class InlineStyleRuleDef
+  extends loom.StyleRuleDef
+  implements Destroyable {
+
+  public destroy = Destroyable.make();
+
+  public constructor(style: CSSStyleDeclaration) {
+    super('(inline)', new InlineStyleDeclarationDef(style));
+
+    this.destroy.do(this.style);
+  }
+}
+
+class InlineStyleDeclarationDef extends loom.StyleDeclarationDef {
+  public constructor(private readonly style: CSSStyleDeclaration) {
+    super();
+
+    this.watch({
+      set: (key, value) => style.setProperty(key, value),
+      delete: key => style.removeProperty(key)
+    });
+
+    this.update();
+  }
+
+  public update(): void {
+    const keys = new Set(this.keys());
+    for (let i = 0; i < this.style.length; i++) {
+      const key = this.style.item(i);
+      this.set(key, this.style.getPropertyValue(key));
+      keys.delete(key);
+    }
+    for (const removed of keys) {
+      this.delete(removed);
+    }
+  }
 }
